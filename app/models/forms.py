1043d270e27c26f9d4d7bd28f18264a63f8a8adb
Tomas Herculano <tomasherculano@gmail.com>
2020-11-01 17:28:37 -0300

    Projeto atualzado 011120 2

---
+++ app/models/forms.py
@@ -0,0 +1,35 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    username = StringField("username", validators=[DataRequired()])
+    password = PasswordField("password", validators=[DataRequired()])
+    remember_me = BooleanField("remember_me")
+
+#-----------Users----------------
+
+class RegisterForm(FlaskForm):
+    name = StringField("name", validators=[DataRequired()])
+    email = StringField("password", validators=[DataRequired()])
+    username = StringField("username", validators=[DataRequired()])
+    password = PasswordField("password", validators=[DataRequired()])
+
+class DeleteForm(FlaskForm):
+    username = StringField("username", validators=[DataRequired()])
+
+class ReadForm(FlaskForm):
+    username = StringField("username", validators=[DataRequired()])
+
+class UpdateForm(FlaskForm):
+    #username = StringField("username", validators=[DataRequired()])
+    name = StringField("name", validators=[DataRequired()])
+    email = StringField("password", validators=[DataRequired()])
+    password = PasswordField("password", validators=[DataRequired()])
+
+#-----------Released----------------
+
+class RegisterFormReleased(FlaskForm):
+    name = StringField("name", validators=[DataRequired()])
+    cpf = StringField("cpf", validators=[DataRequired()])
+
